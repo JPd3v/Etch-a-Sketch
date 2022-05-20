198327c171bb17abf3117b6ce86d943b92b17c6d
JPd3v <>
2022-05-20 03:34:20 -0300

    added javascript
    added function to randomly select a color and pint the grid with
    it and a function to change between the random color and a selected
    color

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 createGrid(16)
-
+changecolor()
+toggleActive()
+getNumber()
 function createGrid(number) {
-    deleteGrid()
-    let container = document.querySelector('.container');
+    if (number > 0) {
+        deleteGrid()
+        let container = document.querySelector('.container');
 
-    for (let i = 0; i < Math.pow(number, 2); i++) {
-        let square = document.createElement('div');
-        square.classList.add('grid-square');
-        container.appendChild(square);
-    }
-    changecolor()
-    modifySquaresWidth(number)
+        for (let i = 0; i < Math.pow(number, 2); i++) {
+            let square = document.createElement('div');
+            square.classList.add('grid-square');
+            container.appendChild(square);
+        }
+        changecolor()
+        toggleActive()
+        modifySquaresWidth(number)
+    } else { return }
 }
 
-getNumber()
+
 function getNumber() {
     let button = document.querySelector(".btn-size")
     button.addEventListener("click", function question() {
@@ -23,7 +28,7 @@ function getNumber() {
         } else if (size >= 31) {
             return question()
         }
-        
+
     })
 }
 
@@ -37,7 +42,39 @@ function changecolor() {
     })
 }
 
+function rainbowColor() {
 
+    let squares = document.querySelectorAll('.grid-square');
+    let color = () => { return `rgb(${Math.floor(Math.random() * 255 + 1)},${Math.floor(Math.random() * 255 + 1)},${Math.floor(Math.random() * 255 + 1)})` }
+    squares.forEach(square => {
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = color()
+        })
+    })
+}
+
+let color = document.querySelector(".btn-color")
+let rainbow = document.querySelector(".btn-rainbow")
+color.addEventListener("click", toggleActive)
+rainbow.addEventListener("click", toggleActive)
+function toggleActive() {
+    let color = document.querySelector(".btn-color")
+    let rainbow = document.querySelector(".btn-rainbow")
+    
+    if (color.classList.contains("active")) {
+        rainbow.addEventListener("click", () => {
+            color.classList.remove("active")
+            rainbow.classList.add("active")
+            return rainbowColor()
+        })
+    } else if (rainbow.classList.contains("active")) {
+        color.addEventListener("click", () => {
+            rainbow.classList.remove("active")
+            color.classList.add("active")
+            return changecolor()
+        })
+    }
+}
 
 function getSquaresWidth(number) {
     let containerWidth = document.querySelector('.container').offsetWidth
@@ -45,14 +82,13 @@ function getSquaresWidth(number) {
 }
 
 function modifySquaresWidth(number) {
-    
     let squares = document.querySelectorAll('.grid-square')
     squares.forEach(square => {
         square.style.minWidth = `${getSquaresWidth(number)}px`
     })
 }
 
-function deleteGrid(){
+function deleteGrid() {
     let container = document.querySelector('.container');
     while (container.lastChild) {
         container.removeChild(container.lastChild)
