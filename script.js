a56b1e0e114923b4183c7ab354ae87721303215d
JPd3v <>
2022-05-19 23:57:57 -0300

    added some javascript functions
    added functions for creating dinamic grids sizes

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-// function createGrid(number) {
-//     let container = document.querySelector('.container');
+function createGrid(number) {
+    let container = document.querySelector('.container');
 
+    for (let i = 0; i < Math.pow(number, 2); i++) {
+        let square = document.createElement('div');
+        square.classList.add('grid-square');
+        container.appendChild(square);
+    }
+    changecolor()
+    modifySquaresWidth(number)
+}
 
-//     for (let i = 0; i < Math.pow(number, 2); i++) {
-//         let square = document.createElement('div');
-//         square.classList.add('grid-square');
-//         container.appendChild(square);
-//     }
-// }
-
-originalGrid()
-function originalGrid() {
+getNumber()
+function getNumber() {
+    let button = document.querySelector(".btn-size")
+    button.addEventListener("click", function question() {
+        let size = Number(prompt("introduce the grid size (1 to 30)"))
+        if (size <= 30) {
+            return createGrid(size)
+        } else if (size >= 31) {
+            return question()
+        }
+        
+    })
+}
+// createOriginalGrid()
+function createOriginalGrid() {
     let container = document.querySelector('.container');
     for (let i = 0; i < 256; i++) {
         let square = document.createElement('div');
@@ -20,11 +34,25 @@ function originalGrid() {
 }
 
 changecolor()
-function changecolor(){
+function changecolor() {
     let squares = document.querySelectorAll('.grid-square');
     squares.forEach(square => {
-        square.addEventListener("mouseover",(e)=>{
-            e.target.classList.add("color")
+        square.addEventListener("mouseover", (e) => {
+        //  e.target.classList.add("color")
+            e.target.style.backgroundColor ="blue"
         })
+    })
+}
+
+function getSquaresWidth(number) {
+    let containerWidth = document.querySelector('.container').offsetWidth
+    return containerWidth / number
+}
+
+function modifySquaresWidth(number) {
+    
+    let squares = document.querySelectorAll('.grid-square')
+    squares.forEach(square => {
+        square.style.minWidth = `${getSquaresWidth(number)}px`
     })
 }
